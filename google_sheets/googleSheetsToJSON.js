910a79d723cc4bab44a71ab02582e2f819f8442b
jswelsh <james.sinclair.welsh@gmail.com>
2020-11-01 14:35:22 -0800

    set up

--- google_sheets/googleSheetsToJSON.js
+++ google_sheets/googleSheetsToJSON.js
@@ -1,1 +1,27 @@
-//test
+ // Includes functions for exporting active sheet or all sheets as JSON object (also Python object syntax compatible).
+// Tweak the makePrettyJSON_ function to customize what kind of JSON to export.
+
+var FORMAT_ONELINE   = 'One-line';
+var FORMAT_MULTILINE = 'Multi-line';
+var FORMAT_PRETTY    = 'Pretty';
+
+var LANGUAGE_JS      = 'JavaScript';
+var LANGUAGE_PYTHON  = 'Python';
+
+var STRUCTURE_LIST = 'List';
+var STRUCTURE_HASH = 'Hash (keyed by "id" column)';
+
+/* Defaults for this particular spreadsheet, change as desired */
+var DEFAULT_FORMAT = FORMAT_PRETTY;
+var DEFAULT_LANGUAGE = LANGUAGE_JS;
+var DEFAULT_STRUCTURE = STRUCTURE_LIST;
+
+
+function onOpen() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var menuEntries = [
+    {name: "Export JSON for this sheet", functionName: "exportSheet"},
+    {name: "Export JSON for all sheets", functionName: "exportAllSheets"}
+  ];
+  ss.addMenu("Export JSON", menuEntries);
+}
